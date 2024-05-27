1d151c8e2e06b9bdc433579422d61a3d8ab74dff
Christopher A. Quinto <141619401+ChrisQuint0@users.noreply.github.com>
2024-05-28 01:40:19 +0800

    Updated Main.java
    Added scroll feature and change the UI to the actual UI

--- grubs-case-study/grubs-1.0/src/com/quintogoto/grubs/Main.java
+++ grubs-case-study/grubs-1.0/src/com/quintogoto/grubs/Main.java
@@ -3,13 +3,15 @@ package com.quintogoto.grubs;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
-public class Main extends JFrame{
+
+public class Main extends JFrame {
     protected JLayeredPane layeredPane;
 
-    public Main(){
-        setTitle("Grubs");
-        setSize(600, 900);
-        setResizable(false); // Making the JFrame not resizable
+    public Main() {
+//        setTitle("Grubs");
+//        setSize(600, 800);
+//      setResizable(false); // Making the JFrame not resizable
+        JFrame frame = new JFrame("Grubs");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // When the User Clicked Close, The Window will Close
         // Create a KeyStroke for the ESC key
         KeyStroke escapeKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
@@ -30,18 +32,30 @@ public class Main extends JFrame{
             }
         });
 
+        frame.setSize(600, 800);
+        frame.setResizable(false); // Making the JFrame not resizable
+
+
         layeredPane = new JLayeredPane(); // Creating a JLayeredPane to manage layers of components
-        setContentPane(layeredPane); // Setting the content pane of the JFrame to the JLayeredPane
+        layeredPane.setPreferredSize(new java.awt.Dimension(600, 1200)); // Example size
+        layeredPane.setLayout(null);
+         // Setting the content pane of the JFrame to the JLayeredPane
 
+        // Wrap the JLayeredPane in a JScrollPane
+        JScrollPane scrollPane = new JScrollPane(layeredPane);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 
+        frame.setContentPane(scrollPane);
 
         Menu menuObj = new Menu();
-        menuObj.setUpUI(layeredPane);
-        setVisible(true);
+        menuObj.setUpUI(layeredPane); //Displaying the Menu UI
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        // Using SwingUtilities.invokeLater to run the GUI code on the Event Dispatch Thread
+        // Using SwingUtilities.invokeLater to run the GUI code on the Event Dispatch
+        // Thread
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
